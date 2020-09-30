0753b5d02fc71e3ee1ed4bf4e5b36e92276f9210
mikealessi24 <60523720+mikealessi24@users.noreply.github.com>
2020-09-29 21:40:33 -0400

    created reducers folder and the todoReducer

---
+++ src/reducers/todoReducer.js
@@ -0,0 +1,14 @@
+import { ADD_TODO, TOGGLE_TODO, SET_VISIBILITY_FILTER } from "../actions";
+
+export const todoReducer = (state = [], action) => {
+  switch (action.type) {
+    case ADD_TODO:
+      return [...state, { text: action.text, id: action.id, completed: false }];
+    case TOGGLE_TODO:
+      return state.map((todo) =>
+        todo.id === action.id ? { ...todo, completed: !todo.completed } : todo
+      );
+    default:
+      return state;
+  }
+};
