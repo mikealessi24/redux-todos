2f3b9601162f907c30358a8fd583421c502a2bf4
mikealessi24 <60523720+mikealessi24@users.noreply.github.com>
2020-09-29 23:15:16 -0400

    added necessary components to app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
-
+import VisibleTodoList from "./containers/VisibleTodoList";
+import AddTodo from "./containers/AddTodo";
+import Footer from "./components/Footer";
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <AddTodo />
+      <VisibleTodoList />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
